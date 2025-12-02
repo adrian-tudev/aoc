fb9ab5f9fdcd0a0ef04c528ca806561ce73e9f5d
Adrian Tudev <tudev@chalmers.se>
2025-12-02 09:10:32 +0100

    day 2 solutions (pretty ugly)

---
+++ d2/sol.py
@@ -0,0 +1,62 @@
+import importlib.util
+from importlib.machinery import ModuleSpec
+import sys
+from pathlib import Path
+from types import ModuleType
+
+parser_path: Path = Path(__file__).resolve().parents[2] / "parser.py"
+spec: ModuleSpec | None = importlib.util.spec_from_file_location("parser", parser_path)
+if spec is None: raise ImportError(f"Could not load spec for parser module at {parser_path}")
+parser: ModuleType = importlib.util.module_from_spec(spec)
+sys.modules["parser"] = parser  
+spec.loader.exec_module(parser)  # type: ignore
+
+from parser import Part, parse_part # type: ignore 
+
+def sol(part: Part, rngs: list[tuple[int, int]]) -> int:
+  ans: int = 0
+
+  for rng in rngs:
+    mem = {}
+    for i in range(1, 100000):
+      id: str | None = None
+
+      if part == Part.ONE:
+        # PART ONE
+        id = str(i) + str(i)
+        n = int(id)
+        if n <= rng[1] and n >= rng[0]:
+          ans += n
+
+      elif part == Part.TWO:
+        # PART TWO
+        upper = len(str(rng[1]))
+        for j in range(2, upper + 1):
+          id = str(i)
+          id *= j
+
+          if id in mem: continue
+          n = int(id)
+          if n <= rng[1] and n >= rng[0]:
+            ans += n
+            mem[id] = True
+            break
+
+      assert id != None
+
+  return ans
+
+def main():
+  contents: str | None = None
+  with open("in.txt", "r") as f:
+    contents = f.read()
+  formatted: list[tuple[int, int]] = []
+  for rng in contents.strip().split(','):
+    fst: int = int(rng.split('-')[0])
+    snd: int = int(rng.split('-')[1])
+    formatted.append((fst, snd))
+
+  print(sol(parse_part(), formatted))
+
+if __name__ == "__main__":
+  main()
